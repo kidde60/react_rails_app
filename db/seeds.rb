096f462f4278bf268eb9d77a2d69a089205a9341
kidde60 <kgeorgewilliam60@.com>
2023-04-05 23:41:11 +0300

    Add api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+messages = Message.create([
+    {greating: 'Hello'},
+    {greating: 'Haalo'},
+    {greating: 'Hey'},
+    {greating: 'Asalam Alaikum'},
+    {greating: 'Good morning'},
+])
